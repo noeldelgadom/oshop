83a87ce8cf11c44839618c3199f7b0898041949f
Noel Delgado <noeldelgadom@myhost.local>
2017-09-21 12:43:59 -0500

    297. showing or hiding the admin links

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,15 +1,21 @@
+import { UserService } from './user.service';
+import { AppUser } from './models/app-user';
 import { Observable } from 'rxjs/Observable';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase';
 import { ActivatedRoute } from '@angular/router';
+import 'rxjs/add/operator/switchMap';
 
 @Injectable()
 export class AuthService {
   user$: Observable<firebase.User>;
 
-  constructor(private afAuth: AngularFireAuth, private route: ActivatedRoute) {
-    this.user$ = afAuth.authState;
+  constructor(
+    private userService: UserService,
+    private afAuth: AngularFireAuth,
+    private route: ActivatedRoute) {
+      this.user$ = afAuth.authState;
   }
 
   login() {
@@ -21,5 +27,10 @@ export class AuthService {
 
   logout() {
     this.afAuth.auth.signOut();
+  }
+
+  get appUser$(): Observable<AppUser> {
+    return this.user$
+      .switchMap(user => this.userService.get(user.uid))
   }
 }
