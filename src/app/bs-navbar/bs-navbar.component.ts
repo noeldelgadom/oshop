8d85868788e456784247d326f6f0d0b050f5bfe5
Noel Delgado <noeldelgadom@myhost.local>
2017-09-25 16:45:33 -0500

    337. refactoring: creating a rich model

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,3 +1,5 @@
+import { ShoppingCart } from './../models/shopping-cart';
+import { Observable } from 'rxjs/Observable';
 import { ShoppingCartService } from './../shopping-cart.service';
 import { AppUser } from './../models/app-user';
 import { AuthService } from './../auth.service';
@@ -10,7 +12,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BsNavbarComponent implements OnInit {
   appUser: AppUser;
-  shoppingCartItemCount: number;
+  cart$: Observable<ShoppingCart>;
 
   constructor(
     private auth: AuthService,
@@ -18,13 +20,7 @@ export class BsNavbarComponent implements OnInit {
 
   async ngOnInit() {
     this.auth.appUser$.subscribe(appUser => this.appUser = appUser);
-    
-    let cart$ = await this.shoppingCartService.getCart();
-    cart$.subscribe(cart => {
-      this.shoppingCartItemCount = 0;
-      for (let productId in cart.items) 
-        this.shoppingCartItemCount += cart.items[productId].quantity;
-    });
+    this.cart$ = await this.shoppingCartService.getCart();  
   }
 
   logout() {
