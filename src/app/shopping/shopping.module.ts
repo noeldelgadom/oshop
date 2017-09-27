6480ce9206d455bffd3a3dce7a5e33f1688a83ff
Noel Delgado <noeldelgadom@myhost.local>
2017-09-27 11:53:08 -0500

    372. importing and exporting modules

--- src/app/shopping/shopping.module.ts
+++ src/app/shopping/shopping.module.ts
@@ -1,23 +1,20 @@
-import { SharedModule } from './../shared/shared.module';
-import { AuthGuard } from './../shared/services/auth-guard.service';
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { ShippingFormComponent } from './components/shipping-form/shipping-form.component';
-import { ShoppingCartSummaryComponent } from './components/shopping-cart-summary/shopping-cart-summary.component';
-import { ProductFilterComponent } from './components/products/product-filter/product-filter.component';
+
+import { AuthGuard } from './../shared/services/auth-guard.service';
+import { SharedModule } from './../shared/shared.module';
+import { CheckOutComponent } from './components/check-out/check-out.component';
 import { MyOrdersComponent } from './components/my-orders/my-orders.component';
 import { OrderSuccessComponent } from './components/order-success/order-success.component';
-import { CheckOutComponent } from './components/check-out/check-out.component';
-import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { ProductFilterComponent } from './components/products/product-filter/product-filter.component';
 import { ProductsComponent } from './components/products/products.component';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { ShippingFormComponent } from './components/shipping-form/shipping-form.component';
+import { ShoppingCartSummaryComponent } from './components/shopping-cart-summary/shopping-cart-summary.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
 
 @NgModule({
   imports: [
     SharedModule,
-    CommonModule,
-    FormsModule,
     RouterModule.forChild([
       { path: 'products', component: ProductsComponent },
       { path: 'shopping-cart', component: ShoppingCartComponent },
